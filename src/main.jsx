65570a88513d0a5457031a08713e04b91ef3c96a
SK-Washif <washiffishal@gmail.com>
2024-10-20 00:18:12 +0600

    path linked header part added

--- src/main.jsx
+++ src/main.jsx
@@ -8,22 +8,40 @@ import {
 } from "react-router-dom";
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
+import Home from './components/Home/Home.jsx';
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App></App>,
-  },
-  {
-    path: '/about',
-    element: <About></About>
-  },
-  {
-    path: '/contact',
-    element: <Contact></Contact>
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App></App>,
+//   },
+//   {
+//     path: '/about',
+//     element: <About></About>
+//   },
+//   {
+//     path: '/contact',
+//     element: <Contact></Contact>
+//   }
+// ]);
+
+const router =createBrowserRouter([
+ {
+  path: '/',
+  element: <Home></Home>,
+  children: [
+    {
+      path: 'about',
+      element: <About></About>
+    },
+    {
+      path: 'contact',
+      element: <Contact></Contact>
+    }
+  ]
+ }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
